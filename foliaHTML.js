749f59343d0c82db582f4a234886cae1f139eb51
ProgramistaZpolski <piotrekbadelek@gmail.com>
2020-10-31 11:00:15 +0100

    nowe tagi

--- foliaHTML.js
+++ foliaHTML.js
@@ -2,7 +2,7 @@
 // by liskqu, ProgramistaZpolski
 let CSScustomURL;
 
-addCSS = function (css = "https://cdn.jsdelivr.net/gh/lisqu16/foliaHTML@0.1.2/css.css") {
+addCSS = function (css = "css.css") {
     cssE = document.createElement("link");
     var attrbs = [{ "attrb": "rel", "value": "stylesheet" }, { "attrb": "type", "value": "text/css" },
     { "attrb": "href", "value": css }];
@@ -46,3 +46,89 @@ function sc(curl) {
     addCSScustomURL();
     loadTags();
 }
+
+
+/* Web Components */
+class Obrazek extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        const wrapper = document.createElement('span');
+        wrapper.setAttribute('class', 'wrapper');
+
+        let imgLink;
+        console.log(this.hasAttribute('plik'));
+        if (this.hasAttribute('plik')) {
+            imgLink = this.getAttribute('plik');
+        } else {
+            imgLink = 'https://via.placeholder.com/300/09f/fff.webp';
+        }
+        
+
+        const img = document.createElement('img');
+        img.src = imgLink;
+        wrapper.appendChild(img);
+
+        /*const style = document.createElement('style');
+        console.log(style.isConnected);*/
+
+
+        /*style.textContent = `
+        img {
+          width: 1.2rem;
+        }
+      `;*/
+
+        /*shadow.appendChild(style);*/
+        shadow.appendChild(wrapper);
+    }
+}
+class Odnosnik extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        const wrapper = document.createElement('span');
+        wrapper.setAttribute('class', 'wrapper');
+
+        let Linkacz;
+        console.log(this.hasAttribute('strona'));
+        if (this.hasAttribute('strona')) {
+            Linkacz = this.getAttribute('strona');
+        } else {
+            Linkacz = '#';
+        }
+        let tekst;
+        console.log(this.hasAttribute('tekst'));
+        if (this.hasAttribute('tekst')) {
+            tekst = this.getAttribute('tekst');
+        } else {
+            tekst = ' ';
+        }
+
+        const hyperlink = document.createElement('a');
+        hyperlink.href = Linkacz;
+        hyperlink.innerText = tekst;
+        wrapper.appendChild(hyperlink);
+
+        const style = document.createElement('style');
+        console.log(style.isConnected);
+
+
+        style.textContent = `
+        a {
+          color: #5e81ac;
+        }
+      `;
+
+        shadow.appendChild(style);
+        shadow.appendChild(wrapper);
+    }
+}
+
+
+customElements.define('obrazek-folia', Obrazek);
+customElements.define('link-folia', Odnosnik);
